8e611a094a691884d04c2c0948462dbd87ef4a93
Ankit Mishra <56253430+ankitmi9811@users.noreply.github.com>
2024-09-04 07:09:51 +0000

    commit done

---
+++ main.java
@@ -0,0 +1,14 @@
+public class main{
+    public static void main(String args[])
+    {
+        int count;
+        for(count =0; count<10; ++count)
+        {
+            System.out.print("#");
+            if(count>6)
+            {continue;}
+            System.out.print(count);
+        }
+
+    }
+}
